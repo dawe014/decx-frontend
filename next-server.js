66c428fee0c6489556aaa3326839a48ea6995f4e
dawe014 <dawittamiru014@gmail.com>
2025-07-28 12:01:13 +0300

    updated

--- next-server.js
+++ next-server.js
@@ -1,24 +1,21 @@
-import express from "express";
-import next from "next";
 import { createServer } from "http";
 import { parse } from "url";
+import next from "next";
 
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
-
-  server.all("*", (req, res) => {
+  createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    return handle(req, res, parsedUrl);
-  });
-
-  // cPanel provides the PORT environment variable.
-  const port = process.env.PORT || 3000;
-
-  createServer(server).listen(port, (err) => {
-    if (err) throw err;
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? "development" : process.env.NODE_ENV
+      }`
+    );
   });
 });
